ccd1a8a80c746156a494ef36c42d5007f3e9d80d
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-10-30 17:12:57 +0100

    creata migration per la relazione 1 to many

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
                                 <a class="nav-link" href="{{ route('admin.projects.index') }}">Portfolio</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.types.index') }}">Generi</a>
+                                <a class="nav-link" href="{{ route('admin.types.index') }}">Genere</a>
                             </li>
                         </ul>
                         <form method="POST" action="{{ route('logout') }}">
